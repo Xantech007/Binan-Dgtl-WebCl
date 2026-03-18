f6635af41314d46791d44e404df0ab2342e36950
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-03-18 02:23:06 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "binance-digital-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.php",
+        "/withdraw.php",
+        "/recharge.php"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
